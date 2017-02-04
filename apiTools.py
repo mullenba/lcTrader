1d5fe510a0f6fc32eab443eaa72ca64dd4363e94
mullenba <mullenba@gmail.com>
2017-02-04 16:39:59 -0500

    Still attempting first commit.

---
+++ apiTools.py
@@ -0,0 +1,46 @@
+import requests, pickle
+
+
+
+
+
+def getCurrentNotes():
+    url = 'https://resources.lendingclub.com/SecondaryMarketAllNotes.csv'
+
+    print("Sending request: ", url)
+    r = requests.get(url, headers=headers, stream=True)
+
+    with open("currentNotes.csv", 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+    return "currentNotes.csv"
+
+def availableCash(accountNumber):
+    url = "https://api.lendingclub.com/api/investor/v1/accounts/{}/availablecash"
+    
+    with open("authkeys.pkl", "rb") as f:
+        headers = pickle.load(f)
+
+    r = requests.get(url.format(accountNumber), headers=headers)
+
+    rObj = r.json()
+
+    return rObj['availableCash']
+
+def saveHeaders(authKey):
+    headers = {'Content-Type': 'application/json', 'Authorization':authKey}
+
+    with open("authkeys.pkl", "wb") as f:
+        pickle.dump(headers, f)
+
+
+    return
+
+
+
+if __name__ == "__main__":
+    #saveHeaders('XXXXXXXXXXXXXXX')
+
+    print(availableCash(101133232))
